2f59782416aa139c6f585112988b43af56a051a5
tanya <tanuceerveevna@gmail.com>
2022-11-17 23:29:59 +0300

    changed map page

--- lib/Pages/mapPage.dart
+++ lib/Pages/mapPage.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
@@ -12,50 +13,139 @@ class MapPageScreen extends StatefulWidget {
 }
 
 class MapPageScreenState extends State<MapPageScreen> {
-
   Completer<GoogleMapController> _controller = Completer();
+  BitmapDescriptor? zalupa;
+  bool isLoading = true;
+  CustomInfoWindowController _customInfoWindowController =
+      CustomInfoWindowController();
+  final LatLng _latLng = LatLng(57.627732, 39.851561);
 
-@override
+  @override
   void initState() {
+    iconLoading();
     getPosition();
     super.initState();
   }
 
-  void getPosition() async{
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  void iconLoading() async {
+    zalupa = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(), 'assets/marker.png');
+    isLoading = false;
+    setState(() {});
+  }
+
+  void getPosition() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
     print(position.latitude);
     print(position.longitude);
   }
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+    target: LatLng(57.627732, 39.851561),
+    zoom: 17.4746,
   );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-          child: GoogleMap(initialCameraPosition: _kGooglePlex,
-            onMapCreated: (GoogleMapController controller) {
-              _controller.complete(controller);
-            },)
-
-      ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 160),
-        child: FloatingActionButton.extended(
-          onPressed: _goToTheLake,
-          label: Text('To the lake!'),
-          icon: Icon(Icons.directions_boat),
-        ),
-      ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Stack(
+              children: [
+                GoogleMap(
+                  onTap: (position) {
+                    _customInfoWindowController.hideInfoWindow!();
+                  },
+                  onCameraMove: (position) {
+                    _customInfoWindowController.onCameraMove!();
+                  },
+                  initialCameraPosition: _kGooglePlex,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                    _customInfoWindowController.googleMapController =
+                        controller;
+                  },
+                  markers: {
+                    Marker(
+                      markerId: MarkerId('1'),
+                      position: LatLng(57.627732, 39.851561),
+                      icon: zalupa!,
+                      onTap: () {
+                        _customInfoWindowController.addInfoWindow!(
+                          Container(
+                              decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset('assets/code.png'),
+                                      SizedBox(
+                                        width: 8.0,
+                                      ),
+                                      Flexible(
+                                        child: Column(
+                                          children: [
+                                            Text('QR-код'),
+                                            Text('Информация о ЖК “Династия”',
+                                            style: TextStyle(
+                                              fontSize: 10
+                                            ),)
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                              ),
+                            ),
+                          _latLng,
+                        );
+                      },
+                    ),
+                    Marker(
+                      markerId: MarkerId('2'),
+                      position: LatLng(57.627683, 39.852203),
+                      icon: zalupa!,
+                    ),
+                  },
+                ),
+                CustomInfoWindow(
+                  controller: _customInfoWindowController,
+                  height: 75,
+                  width: 150,
+                  offset: 50,
+                ),
+              ],
+            ),
     );
   }
 
   Future<void> _goToTheLake() async {
-  final GoogleMapController controller = await _controller.future;
-  controller.animateCamera(CameraUpdate.newCameraPosition(_kGooglePlex));
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kGooglePlex));
   }
-
 }
